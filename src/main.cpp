9bab4e780d3202d40801c620f81ce36324d9a006
yazdan-iot <yazdan.electrical.eng@gmail.com>
2026-07-16 03:09:27 +0330

    wip: dht22 freeRTOS task implementation (resolving sensor timeouts)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
+#include <SimpleDHT.h>
 
+// ---------------- Soil moisture -----------------
 const int SENSOR_POWER_PIN = 6;   // GPIO that powers the sensor on/off
 const int SENSOR_ADC_PIN   = 4;   // GPIO that reads the analog moisture value
 
@@ -7,11 +9,15 @@ const int RAW_DRY = 3500;   // raw value when soil is dry (HIGH now)
 const int RAW_WET = 0;      // raw value when fully wet (LOW now)
 
 volatile int soilPercent = -1;    // latest moisture reading as a percentage, shared across tasks
+const unsigned long READ_INTERVAL_MS = 1800000; // how often to read the sensor (ms); 1800000 = 30 min for production
 
 SemaphoreHandle_t serialMutex;    // prevents multiple tasks from printing to Serial at the same time
 
-const unsigned long READ_INTERVAL_MS = 1800000; // how often to read the sensor (ms); 1800000 = 30 min for production
+// ---------------- DHT22 -----------------
+const int DHT_PIN = 18; // GPIO that reads the DHT22 sensor
+SimpleDHT22 dht22(DHT_PIN); // Create an instance of the DHT22 sensor
 
+// --------------- Soil moisture sensor task -----------------
 int rawToPercent(int raw) {
   int percent = map(raw, RAW_DRY, RAW_WET, 0, 100);
   return constrain(percent, 0, 100);
@@ -48,13 +54,40 @@ void soilSensorTask(void *parameter) {
   }
 }
 
+
+// ---------------- DHT22 sensor task -----------------
+void dhtSensorTask(void *parameter) {
+  for (;;) {
+    float temperature = 0;
+    float humidity = 0;
+    int err = SimpleDHTErrSuccess;
+
+    // read the DHT22 sensor; if it fails, print an error and wait 2.5 seconds before retrying
+    if ((err = dht22.read2(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+      Serial.print("Read DHT22 failed, err=");
+      Serial.println(err);
+      
+      vTaskDelay(pdMS_TO_TICKS(2500));
+      continue;
+    } 
+
+    xSemaphoreTake(serialMutex, portMAX_DELAY);
+    Serial.printf("Temperature: %.2f °C | Humidity: %.2f %%\n", temperature, humidity);
+    xSemaphoreGive(serialMutex);
+
+    vTaskDelay(pdMS_TO_TICKS(3000)); // Delay for 3 seconds between readings
+  }
+}
+
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
   Serial.println("Starting soil moisture sensor test (FreeRTOS)...");
 
   serialMutex = xSemaphoreCreateMutex();
-  xTaskCreatePinnedToCore(soilSensorTask, "SoilSensorTask", 2048, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(soilSensorTask, "SoilSensorTask", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(dhtSensorTask, "DHTSensorTask", 4096, NULL, 2, NULL, 1);
 }
 
 void loop() {
